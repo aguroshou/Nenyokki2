805b488cd80523592d5f6daaa84147adebd57410
DESKTOP-QTIMD38\aguro <aguroshou0413@gmail.com>
2023-02-16 22:58:13 +0900

    UE5の空プロジェクトを作成しました。

---
+++ Source/Nenyokki2/Nenyokki2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Nenyokki2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NENYOKKI2_API ANenyokki2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
